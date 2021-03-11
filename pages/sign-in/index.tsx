f7ba78e6c4202d0e16a2215523ccef8ab0c0bbc2
mingyumkim93 <43638576+mingyumkim93@users.noreply.github.com>
2021-03-11 21:17:05 +0000

    Head in sign in/up pages

--- pages/sign-in/index.tsx
+++ pages/sign-in/index.tsx
@@ -1,9 +1,14 @@
+import Head from "next/head";
 import MyLayout from "components/layouts/MyLayout";
 import Page from "types/Page";
 
 const SignIn: Page = () => {
   return (
     <>
+      <Head>
+        <title>sign-in</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div>Sign in page</div>
     </>
   );
--- pages/sign-up/index.tsx
+++ pages/sign-up/index.tsx
@@ -1,9 +1,14 @@
+import Head from "next/head";
 import MyLayout from "components/layouts/MyLayout";
 import Page from "types/Page";
 
 const SignUp: Page = () => {
   return (
     <>
+      <Head>
+        <title>sign-up</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div>Sign up page</div>
     </>
   );
