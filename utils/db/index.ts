0ce1cc0970efc15550f1a882004f26fa2b4ac9e2
mingyumkim93 <43638576+mingyumkim93@users.noreply.github.com>
2021-03-15 15:10:35 +0000

    firestore setup

---
+++ utils/db/index.ts
@@ -0,0 +1,13 @@
+import admin, { ServiceAccount } from "firebase-admin";
+import serviceAccount from "./serviceAccountKey.json";
+
+if (!admin.apps.length) {
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount as ServiceAccount)
+    });
+  } catch (error) {
+    console.log("Firebase admin initialization error", error.stack);
+  }
+}
+export default admin.firestore();
