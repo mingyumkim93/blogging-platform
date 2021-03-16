29ead36a933276ddcc05dfc56d498e4f9a04b177
mingyumkim93 <43638576+mingyumkim93@users.noreply.github.com>
2021-03-16 13:36:52 +0000

    (photoURL too long) sign-up page with inputs

---
+++ pages/api/users/index.ts
@@ -0,0 +1,25 @@
+import firestore from "utils/firestore";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { uid, email, displayName, photoURL } = req.body;
+    const users = await firestore.collection("users").get();
+    const usersData = users.docs.map((user) => user.data());
+
+    if (usersData.some((user) => user.email === email)) {
+      res.status(400).end();
+    } else {
+      const result = await firestore.collection("users").doc(uid).set({
+        email,
+        displayName,
+        photoURL,
+        created: new Date().toISOString()
+      });
+      console.log(result);
+      res.status(200).json(uid);
+    }
+  } catch (e) {
+    res.status(400).end();
+  }
+};
