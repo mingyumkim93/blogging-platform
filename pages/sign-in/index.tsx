8d13b9df7064020a82c064e7311e3eab427be7f4
mingyumkim93 <43638576+mingyumkim93@users.noreply.github.com>
2021-03-18 09:51:00 +0000

    sign in inputs

--- pages/sign-in/index.tsx
+++ pages/sign-in/index.tsx
@@ -1,15 +1,33 @@
 import Head from "next/head";
 import MyLayout from "components/layouts/MyLayout";
 import Page from "types/Page";
+import { useState } from "react";
+import { useFirebase } from "lib/firebase/FirebaseProvider";
+import { useRouter } from "next/router";
 
 const SignIn: Page = () => {
+  const auth = useFirebase().auth;
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function signin() {
+    auth
+      .signin(email, password)
+      .then(() => router.push("/"))
+      .catch((err) => console.log(err));
+  }
+
   return (
     <div className="page-container">
       <Head>
         <title>sign-in</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>Sign in page</div>
+      <input type="text" onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={() => signin()}>Sign in</button>
     </div>
   );
 };
