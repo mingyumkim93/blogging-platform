01cc4fa79d2b4920d1e8c9e84856546a8a338119
mingyumkim93 <43638576+mingyumkim93@users.noreply.github.com>
2021-03-22 13:28:13 +0000

    removing image input in sign up page

--- pages/sign-up/index.tsx
+++ pages/sign-up/index.tsx
@@ -4,61 +4,31 @@ import Page from "types/Page";
 import { useState } from "react";
 import { useFirebase } from "lib/firebase/FirebaseProvider";
 import axios from "axios";
-import styles from "styles/SignUp.module.scss";
 import { useRouter } from "next/router";
 
 const SignUp: Page = () => {
-  const { auth, storage } = useFirebase();
-  const { user, signup } = auth;
+  const { auth } = useFirebase();
   const router = useRouter();
 
   //TODO: validate
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [displayName, setDisplayName] = useState("");
-  const [photo, setPhoto] = useState<File | null>(null);
-  const [photoPreviewData, setPhotoPreviewData] = useState("");
 
-  async function submitSignUp() {
-    const registeredUser = await signup(email, password);
+  async function signUp() {
+    const registeredUser = await auth.signup(email, password);
     if (registeredUser) {
-      if (photo) {
-        const snapshot = await storage.addPhoto(registeredUser.uid, photo);
-        const downloadURL = (await snapshot.ref.getDownloadURL()) as string;
-        axios.post("/api/users", {
-          uid: registeredUser.uid,
-          email,
-          displayName,
-          photoURL: downloadURL
-        });
-        await registeredUser.updateProfile({
-          displayName,
-          photoURL: downloadURL
-        });
-      }
+      await axios.post("/api/users", {
+        uid: registeredUser.uid,
+        email,
+        displayName
+      });
+      await registeredUser.updateProfile({ displayName });
       router.push("/");
     }
   }
 
-  function openImageInput() {
-    (document.getElementById("image-input") as HTMLInputElement).click();
-  }
-
-  function inputImage() {
-    const imageInput = document.getElementById(
-      "image-input"
-    ) as HTMLInputElement;
-    setPhoto(imageInput.files ? imageInput.files[0] : null);
-
-    if (imageInput.files && imageInput.files[0]) {
-      setPhoto(imageInput.files[0]);
-      const reader = new FileReader();
-      reader.readAsDataURL(imageInput.files[0]);
-      reader.onloadend = () => setPhotoPreviewData(reader.result as string);
-    }
-  }
-
-  if (user) {
+  if (auth.user) {
     router.push("/");
     return <></>;
   }
@@ -83,16 +53,7 @@ const SignUp: Page = () => {
         onChange={(e) => setDisplayName(e.target.value)}
         placeholder="Name"
       />
-      <input
-        className={styles["image-input"]}
-        type="file"
-        id="image-input"
-        accept=".jpg, .jpeg, .png"
-        onChange={() => inputImage()}
-      />
-      <img height="200" src={`${photoPreviewData}`} />
-      <button onClick={() => openImageInput()}>profile photo</button>
-      <button onClick={() => submitSignUp()}>Submit</button>
+      <button onClick={() => signUp()}>Submit</button>
     </div>
   );
 };
