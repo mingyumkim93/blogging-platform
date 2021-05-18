dc4b1bc2f1cd76b40d4d8a5663ae64af89a81ab1
mingyumkim93 <43638576+mingyumkim93@users.noreply.github.com>
2021-05-18 14:46:53 +0100

    simple main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import AppBarLayout from "components/layouts/AppBarLayout";
 import Page from "types/Page";
+import Button from "@material-ui/core/Button";
+
+const EXAMPLE_BLOG_URL = "/blog/mingyum-kim";
 
 const Home: Page = () => (
   <div className="page-container">
@@ -8,7 +11,14 @@ const Home: Page = () => (
       <title>Blogging platform By Mingyum Kim</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    Home
+    <h1>Welcome to blogging platform by Mingyum Kim!</h1>
+    <p>You can create your blog page and edit contents.</p>
+    <Button
+      onClick={() => window.open(EXAMPLE_BLOG_URL)}
+      variant="contained"
+      color="primary">
+      See Example
+    </Button>
   </div>
 );
 
