00b4f13c94eaec4887265cc9ea912b6a9df4266b
MingyumKim <Mingyum93@gmail.com>
2021-06-08 18:51:44 +0300

    fixed error about env variables

--- lib/firebase/firebase-admin/firestore.ts
+++ lib/firebase/firebase-admin/firestore.ts
@@ -1,21 +1,39 @@
 import admin, { ServiceAccount } from "firebase-admin";
 
-const serviceAccount = {
-  type: process.env.SERVICE_ACCOUNT_TYPE,
-  project_id: process.env.SERVICE_ACCOUNT_PROJECT_ID,
-  private_key_id: process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
-  private_key: (process.env.SERVICE_ACCOUNT_PRIVATE_KEY as string).replace(
-    /\\n/g,
-    "\n"
-  ),
-  client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
-  client_id: process.env.SERVICE_ACCOUNT_CLIENT_ID,
-  auth_uri: process.env.SERVICE_ACCOUNT_AUTH_URI,
-  token_uri: process.env.SERVICE_ACCOUNT_TOKEN_URI,
-  auth_provider_x509_cert_url:
-    process.env.SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.SERVICE_ACCOUNT_CLIENT_X509_CERT_URL
-};
+const serviceAccount =
+  process.env.NODE_ENV === "production"
+    ? {
+        type: process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_TYPE,
+        project_id: process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_PROJECT_ID,
+        private_key_id: process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+        private_key: (process.env
+          .NEXT_PUBLIC_SERVICE_ACCOUNT_PRIVATE_KEY as string).replace(
+          /\\n/g,
+          "\n"
+        ),
+        client_email: process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_CLIENT_EMAIL,
+        client_id: process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_CLIENT_ID,
+        auth_uri: process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_AUTH_URI,
+        token_uri: process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_TOKEN_URI,
+        auth_provider_x509_cert_url:
+          process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url:
+          process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL
+      }
+    : {
+        type: process.env.SERVICE_ACCOUNT_TYPE,
+        project_id: process.env.SERVICE_ACCOUNT_PROJECT_ID,
+        private_key_id: process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+        private_key: (process.env
+          .SERVICE_ACCOUNT_PRIVATE_KEY as string).replace(/\\n/g, "\n"),
+        client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+        client_id: process.env.SERVICE_ACCOUNT_CLIENT_ID,
+        auth_uri: process.env.SERVICE_ACCOUNT_AUTH_URI,
+        token_uri: process.env.SERVICE_ACCOUNT_TOKEN_URI,
+        auth_provider_x509_cert_url:
+          process.env.SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.SERVICE_ACCOUNT_CLIENT_X509_CERT_URL
+      };
 
 if (!admin.apps.length) {
   try {
--- lib/firebase/firebase.ts
+++ lib/firebase/firebase.ts
@@ -2,17 +2,24 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/storage";
 
-console.log("api key: ", process.env.FIREBASE_API_KEY);
-console.log("test key: ", process.env.NEXT_PUBLIC_TEST);
-console.log("!!! ", process.env.NODE_ENV);
+const fibaseConfig =
+  process.env.NODE_ENV === "production"
+    ? {
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        appID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
+      }
+    : {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        appID: process.env.FIREBASE_APP_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+      };
 if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    appID: process.env.FIREBASE_APP_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET
-  });
+  firebase.initializeApp(fibaseConfig);
 } else {
   firebase.app();
 }
