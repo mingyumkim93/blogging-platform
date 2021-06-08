bf069a4248ae0ef7049dfdd0a77ef7aa133bd29d
MingyumKim <Mingyum93@gmail.com>
2021-06-08 19:08:36 +0300

    api key test7

--- lib/firebase/firebase-admin/firestore.ts
+++ lib/firebase/firebase-admin/firestore.ts
@@ -6,8 +6,7 @@ const serviceAccount =
         type: process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_TYPE,
         project_id: process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_PROJECT_ID,
         private_key_id: process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
-        private_key: (process.env
-          .SERVICE_ACCOUNT_PRIVATE_KEY as string).replace(/\\n/g, "\n"),
+        private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
         client_email: process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_CLIENT_EMAIL,
         client_id: process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_CLIENT_ID,
         auth_uri: process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_AUTH_URI,
